64cfe2a3461ec7f3327011567e4267ebf28a2d06
imlucky <ali.dabagh7@gmail .com>
2018-12-26 09:53:36 +0330

    first commit

---
+++ src/main/java/tacos/Taco.java
@@ -0,0 +1,34 @@
+package tacos;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.util.Date;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@Entity
+public class Taco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private Date createdAt;
+
+    @NonNull
+    @Size(min = 5, message = "Name must be at least 5 characters long")
+    private String name;
+
+    @ManyToMany(targetEntity = Ingredient.class)
+    @Size(min = 1, message = "You must choose at least 1 ingredient")
+    private List<Ingredient> ingredients;
+
+    @PrePersist
+    void createdAt() {
+        this.createdAt = new Date();
+    }
+}
